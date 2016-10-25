db72ad38e674286e6cf7101c06de0e2d755a22da
AlekhyaMallina-Vedams <alekhyam@biarca.com>
2016-10-25 11:04:57 +0530

    Update checker.py

--- pyflakes/checker.py
+++ pyflakes/checker.py
@@ -2,7 +2,7 @@
 # -*- test-case-name: pyflakes -*--
 # (c) 2005-2010 Divmod, Inc.
 # See LICENSE file for details
-#dfgdfgdfgbdfg
+#dfgdfgdfgbdfgfgthfghfghfgh
 #dfgdfgdfgdfg
 import __builtin__
 import os.path
