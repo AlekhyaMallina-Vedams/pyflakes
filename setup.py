1c39660d4777c30b7e9ac64aa13e89b19f955283
SrinivasDasthagiri <srinivasd@biarca.com>
2016-11-02 16:29:39 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # (c) 2005-2009 Divmod, Inc.  See LICENSE file for details
-#asdasdsaasdasdsadsadsa
+#asdasdsaasdasdsadsadsaasdasdasdsd
 from distutils.core import setup
 
 setup(
